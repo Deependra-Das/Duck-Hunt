3ce84801222a640570801e0c8dfaf60499acdb95
Deependra-Das <iamdeep75@gmail.com>
2024-07-22 19:09:13 +0530

    Bug Fixes

--- Duck-Hunt/Source/Duck/DuckService.cpp
+++ Duck-Hunt/Source/Duck/DuckService.cpp
@@ -172,7 +172,7 @@ namespace Duck
 				if (duck_list[i]->getDuckType() == DuckType::RED)
 				{
 					r_duck_shot++;
-					ServiceLocator::getInstance()->getAnimationService()->spawnAnimationSystem(duck_list[i]->getDuckPosition(), Animation::AnimationType::RED_DUCK_FALL, MovementDirection::DOWN);
+				//	ServiceLocator::getInstance()->getAnimationService()->spawnAnimationSystem(duck_list[i]->getDuckPosition(), Animation::AnimationType::RED_DUCK_FALL, MovementDirection::DOWN);
 					red_duck_position = duck_list[i]->getDuckPosition();
 					destroyDuck(duck_list[i]);
 
@@ -180,17 +180,17 @@ namespace Duck
 				else if (duck_list[i]->getDuckType() == DuckType::BLACK)
 				{
 					b_duck_shot++;
-					ServiceLocator::getInstance()->getAnimationService()->spawnAnimationSystem(duck_list[i]->getDuckPosition(), Animation::AnimationType::BLACK_DUCK_FALL, MovementDirection::DOWN);
+				//	ServiceLocator::getInstance()->getAnimationService()->spawnAnimationSystem(duck_list[i]->getDuckPosition(), Animation::AnimationType::BLACK_DUCK_FALL, MovementDirection::DOWN);
 					destroyDuck(duck_list[i]);
 				}
 			}
 
 		}
 
-	/*	if (r_duck_shot > 0)
+		if (r_duck_shot > 0)
 		{
 			b_duck_shot += killNearbyDucks(red_duck_position);
-		}*/
+		}
 
 		duck_shot = r_duck_shot + b_duck_shot;
 		score = (r_duck_shot * 200) + (b_duck_shot * 100);
